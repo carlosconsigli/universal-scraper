c7bd3871890b099c353a0bd770672591aa8ae147
unknown <carlosconsigli@gmail.com>
2025-06-02 23:03:33 -0300

    modificando postinstall

--- postinstall.sh
+++ postinstall.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "Descargando modelo de spaCy en_core_web_sm..."
-python -m spacy download en_core_web_sm
+py -m spacy download en_core_web_sm
 echo "Modelo descargado correctamente."
 
